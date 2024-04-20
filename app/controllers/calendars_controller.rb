5006639c79fbf6a5a5e41f464d9166da7e0e4f41
swanmask353 <youhei.suwa@gmail.com>
2024-04-20 20:31:57 +0900

    Issue1~3修正（再）

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -43,7 +43,7 @@ class CalendarsController < ApplicationController
        # 曜日の文字列を取得して追加
         wday_str = wdays[wday_num]
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wday_str }
+      days = { month:(@todays_date + x).month, date: (@todays_date + x).day, plans:today_plans, wday: wday_str }
       @week_days.push(days)
     end
   end
